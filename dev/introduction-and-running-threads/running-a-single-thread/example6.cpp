b5dbfe456a86cdaafbc37e7cb6d82d6ac87c7dd0
deepbiolab <lin_xiangtian@outlook.com>
2025-01-03 22:18:28 +0800

    Update development note

---
+++ dev/introduction-and-running-threads/running-a-single-thread/example6.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <thread>
+
+void threadFunction()	{
+	std::this_thread::sleep_for(std::chrono::milliseconds(50));
+	std::cout << "Finished work in thread\n";
+}
+
+int main() {
+	std::thread t(threadFunction);
+	
+	t.detach();
+
+	std::this_thread::sleep_for(std::chrono::milliseconds(50));
+	std::cout << "Finished work in main\n";
+}
